dc03bb79feece3f579725a37ca177ce9ef6e1524
LT <lteran@email.arizona.edu>
2017-12-14 07:39:20 -0800

    Initial commit, DataStructure project library not included in this repository.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,91 @@
+﻿using Algorithms.Sorting;
+using System;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static BubbleSort bubbleSort { get; set; }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool running = true;
+
+                Console.WriteLine("Welcome to the application!");
+                Console.WriteLine("Please wait while we get everything ready...");
+
+                do
+                {
+                    Console.WriteLine("");
+
+                    var input = Console.ReadLine();
+
+                    switch (input)
+                    {
+                        case "bubble-sort":
+                            OpenDialog(Sort.BubbleSort);
+                            break;
+                        case "exit":
+                            running = false;
+                            break;
+                        default:
+                            Console.WriteLine("Did not recognize user input.");
+                            break;
+                    }
+
+                } while (running);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void OpenDialog(Sort type)
+        {
+            switch (type)
+            {
+                case Sort.BubbleSort:
+                    Console.WriteLine("Running BubbleSort Algorithm");
+                    BubbleSort();
+                    break;
+                default: break;
+            }
+        }
+
+        static void BubbleSort()
+        {
+            var running = true;
+
+            do
+            {
+                var input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "isSorted":
+                        if (bubbleSort.IsSorted())
+                            Console.Write("TRUE");
+                        break;
+                    case "delete":
+                    case "sort":
+                    case "add":
+                        Console.WriteLine("Not yet implemented");
+                        break;
+                    case "exit":
+                        running = false;
+                        break;
+                    default: break;
+                }
+            } while (running);
+
+        }
+
+        enum Sort
+        {
+            BubbleSort
+        }
+    }
+}
---
+++ Algorithms/Sorting/BubbleSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting
+{
+    public class BubbleSort
+    {
+        #region Members
+        private int[] Data { get; set; }
+        #endregion
+
+        public BubbleSort(int[] data)
+        {
+            this.Data = data;
+        }
+
+        public void Sort()
+        {
+            for (int i = Data.Length; i > 0; i--)
+            {
+                if (Data[i] < Data[i - 1])
+                {
+                    int tmp = Data[i];
+                    Data[i] = Data[i - 1];
+                    Data[i - 1] = tmp;
+                }
+            }
+        }
+
+        public int[] GetData()
+        {
+            return Data;
+        }
+
+        public bool IsSorted()
+        {
+            for (int i = 0; i < Data.Length - 1; i++)
+            {
+                if (Data[i] > Data[i + 1])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
