2552d0e360ceb72d4a994ea7920a2fd9feab7280
Luis A Teran <lteran@email.arizona.edu>
2019-10-11 08:11:35 -0700

    Refactoring.

--- Algorithms/Sorting/Extensions.cs
+++ Algorithms/Sorting/Extensions.cs
@@ -6,19 +6,8 @@ using System.Threading.Tasks;
 
 namespace Algorithms.Sorting
 {
-    static class Extensions
+    public static class Extensions
     {
-        public static bool IsSorted(this int[] array)
-        {
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                if (array[i] > array[i + 1])
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
+        
     }
 }
